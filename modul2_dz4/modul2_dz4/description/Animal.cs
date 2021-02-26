b4ff4f01839216f0c37c9c98cc6cded06398836d
denys-u <denys.udovik@nure.ua>
2021-02-26 17:21:11 +0200

    Added Squad enam

--- modul2_dz4/modul2_dz4/description/Animal.cs
+++ modul2_dz4/modul2_dz4/description/Animal.cs
@@ -6,10 +6,15 @@ namespace modul2_dz4.description
 {
     public abstract class Animal
     {
-        protected Animal(AnimalType animaltype, double weight)
+        protected Animal(AnimalType animaltype, string name, double weight)
         {
             Weight = weight;
+            Name = name;
         }
         public double Weight { get; }
+
+        public AnimalType animaltype { get; }
+        
+        public string Name { get; }
     }
 }
---
+++ modul2_dz4/modul2_dz4/enums/Squad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul2_dz4.enums
+{
+    public enum Squad
+    {
+        Predator,
+        Artiodactyls,
+        Rodent
+    }
+}

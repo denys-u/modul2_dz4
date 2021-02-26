4e4500603fc3592bebdf41a1a92b264771da0df6
denys-u <denys.udovik@nure.ua>
2021-02-26 21:20:52 +0200

    chto-to dobavil, zachem? ne znajy

---
+++ modul2_dz4/modul2_dz4/Resualt/AnimalEnclosure.cs
@@ -0,0 +1,12 @@
+﻿using modul2_dz4.description;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul2_dz4.Resualt
+{
+    public class AnimalEnclosure
+    {
+        public Animal[] Animals { get; set; }
+    }
+}

d0050f6cf19be668e9db2994e3fb034cd9f9538c
denys-u <denys.udovik@nure.ua>
2021-02-26 16:30:50 +0200

    added first class and enum

---
+++ modul2_dz4/modul2_dz4/description/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using modul2_dz4.enums;
+namespace modul2_dz4.description
+{
+    public abstract class Animal
+    {
+        protected Animal(AnimalType animaltype, double weight)
+        {
+            Weight = weight;
+        }
+        public double Weight { get; }
+    }
+}

8607a9ee549d4c10ab9e07da365a962d24fad8ea
denys-u <denys.udovik@nure.ua>
2021-02-26 18:30:01 +0200

    Added one more Family and 3 types of animals

---
+++ modul2_dz4/modul2_dz4/Animals/Lion.cs
@@ -0,0 +1,16 @@
+﻿using modul2_dz4.description;
+using modul2_dz4.enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul2_dz4.Animals
+{
+    public class Lion : CatsTeam
+    {
+        public Lion(AnimalType animaltype, string name, double weight)
+            : base(animaltype, name, weight)
+        {
+        }
+    }
+}
